770ccd3ae5dadc507d11f39d6830b318f5de66aa
maoxingda <39357378+maoxingda@users.noreply.github.com>
2018-11-19 10:07:31 +0800

    1、建立工程；

---
+++ sgi-stl/sgi-stl/sgi-stl.cpp
@@ -0,0 +1,14 @@
+// sgi-stl.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+//#include "stl_uninitialized.h"
+#include "stl_construct.h"
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	
+	return 0;
+}
+
