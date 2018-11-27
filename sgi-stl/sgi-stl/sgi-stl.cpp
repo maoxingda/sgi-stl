e56a40f19988ab69a54461c2010402aededffbbb
maoxingda <39357378+maoxingda@users.noreply.github.com>
2018-11-27 19:39:53 +0800

    1、实现算法；

--- sgi-stl/sgi-stl/sgi-stl.cpp
+++ sgi-stl/sgi-stl/sgi-stl.cpp
@@ -7,24 +7,40 @@
 #include "deque"
 #include "stack"
 #include "queue"
+#include "algorithm"
 
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	int ia[9] = {0, 1, 2, 3, 4, 8, 9, 3, 5};
-	priority_queue<int> ipq(ia, ia + 9);
+	list<int> il;
+	vector<int> iv;
 
-	for (int i = 0; i < 9 ; i++)
+	for (auto i = 0; i < 5; ++i)
 	{
-		printf("%d, ", ipq.top());
+		il.push_back(i);
+		iv.push_back(i);
 	}
-	printf("\n");
 
-	while (!ipq.empty())
-	{
-		printf("%d, ", ipq.top());
-		ipq.pop();
-	}
+	auto iter1 = find(il.begin(), il.end(), 3);
+	il.insert(iter1, 5);
+
+	auto iter2 = find(iv.begin(), iv.end(), 3);
+	iv.insert(iter2, 5);
+
+	//int ia[9] = {0, 1, 2, 3, 4, 8, 9, 3, 5};
+	//priority_queue<int> ipq(ia, ia + 9);
+
+	//for (int i = 0; i < 9 ; i++)
+	//{
+	//	printf("%d, ", ipq.top());
+	//}
+	//printf("\n");
+
+	//while (!ipq.empty())
+	//{
+	//	printf("%d, ", ipq.top());
+	//	ipq.pop();
+	//}
 	return 0;
 }
 
